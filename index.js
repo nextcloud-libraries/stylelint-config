a13878f71c1ffaa16f7411a66bf1226b84b322bc
Grigorii K. Shartsev <me@shgk.me>
2025-05-22 11:22:32 +0200

    fix(use-logical): ignore properties with `/-(top|bottom)-/`
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- index.js
+++ index.js
@@ -59,6 +59,9 @@ module.exports = {
 					// Position properties with directional suffixes
 					/-top$/,
 					/-bottom$/,
+					// also for e.g. border-bottom-color
+					/-top-/,
+					/-bottom-/,
 					// Size properties
 					'width',
 					'max-width',
